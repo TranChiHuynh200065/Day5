eaf89d2ed82395539498c96d5f513f9c2fca5dbc
TranChiHuynh200065 <huynhtcgcc200065@fpt.edu.vn>
2022-09-27 10:15:59 +0700

    h

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    //__dirname : It will resolve to your project folder.
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    res.send('Hello')
+        //__dirname : It will resolve to your project folder.
 });
-
+/*
 router.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 });
@@ -15,7 +16,7 @@ router.get('/about', function(req, res) {
 router.get('/sitemap', function(req, res) {
     res.sendFile(path.join(__dirname + '/sitemap.html'));
 });
-
+*/
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
